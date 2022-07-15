260ae9c92512afcd43ce436498e7d26eacee9851
Chris Ra <chraab@gmail.com>
2022-07-15 17:09:57 +0200

    Fix NaN test case for patient_normalise

--- tests/test_models.py
+++ tests/test_models.py
@@ -59,7 +59,8 @@ def test_daily_min(test, expected):
         ([[-1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]], ValueError,),
         ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]], None,),
         ([[float('nan'), 2, 3], [4, 5, float('nan')], [float('nan'), float('nan'), float('nan')]],
-         [[0, 0.67, 1], [0.8, 1, 0], [0, 0, 0]]),
+         [[0, 0.67, 1], [0.8, 1, 0], [0, 0, 0]],
+         None),
     ])
 def test_patient_normalise(test, expected, expected_raises):
     """Test normalisation works for arrays of one and positive integers.
